4a230fea151ece452be41d7fb7479fa2db557d81
ML-Mounika <159158311+ML-Mounika@users.noreply.github.com>
2024-03-15 15:07:44 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and Welcome to FlexDeploy this is a Devops Tool!");
+        System.out.println("Hello and Welcome to FlexDeploy this is a Devops Tools!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
 
